c4c292965aec5d37d9b6be04356a121c222e1fd3
Lyoung0789 <Lestermyoung07@gmail.com>
2020-09-22 23:08:56 -0400

    Adding computer play functionality and compare whose the winner

--- app.js
+++ app.js
@@ -13,7 +13,73 @@ const game = () => {
     });
   };
 
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const pHand = document.querySelector(".player-hand");
+    const cHand = document.querySelector(".computer-hand");
+
+    //computer options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        const playerChoice = this.innerText;
+
+        //We will call compareHands
+        console.log("Player:", playerChoice);
+        console.log("Computer:", computerChoice);
+        compareHands(playerChoice, computerChoice);
+      });
+    });
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    const winner = document.querySelector(".winner");
+
+    //checks for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It's a tie!";
+      return;
+    }
+
+    //checks for rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "paper") {
+        winner.textContent = "Computer Wins!";
+        return;
+      } else {
+        winner.textContent = "Player Wins!";
+        return;
+      }
+    }
+
+    if (playerChoice === "paper") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Player Wins!";
+        return;
+      } else {
+        winner.textContent = "Computer Wins!";
+        return;
+      }
+    }
+
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Player Wins!";
+        return;
+      } else {
+        winner.textContent = "Computer Wins!";
+        return;
+      }
+    }
+  };
+
   startGame();
+  playMatch();
 };
 
 game();
